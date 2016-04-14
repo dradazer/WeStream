ef0149ec3b0aa691a81cabbd42de55267b936428
For A Lack Of A Better Name <dradazer@gmail.com>
2016-04-14 15:34:08 +0100

    Added files via upload

---
+++ WeStream/WeStream/DataModel/DataSource.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace WeStream.DataModel
+{
+    public class PicInfo
+    {
+        public string picTitle { get; set; }
+        public DateTime created { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+
+    public class DataSource
+    {
+        private ObservableCollection<PicInfo> _picInfo;
+        const string fileName = "picinfo.json";
+
+        public DataSource()
+        {
+            _picInfo = new ObservableCollection<PicInfo>();
+        }
+
+        public async Task<ObservableCollection<PicInfo>> getPicInfo()
+        {
+            //wait for ensureDataLoaded to be completed then return ObservableCollection _picInfo
+            await ensureDataLoaded();
+            return _picInfo;
+        }
+
+        private async Task ensureDataLoaded()
+        {
+            if (_picInfo.Count == 0)
+                await getPicInfoAsync();
+            return;
+        }
+
+        private async Task getPicInfoAsync()
+        {
+            if (_picInfo.Count != 0)
+                return;
+
+            var jsonSerializer = new DataContractJsonSerializer(typeof(ObservableCollection<PicInfo>));
+
+            try
+            {
+                using (var stream = await ApplicationData.Current.LocalFolder.OpenStreamForReadAsync(fileName))
+                {
+                    //try to read the stream object
+                    _picInfo = (ObservableCollection<PicInfo>)jsonSerializer.ReadObject(stream);
+                }
+            }
+            catch
+            {
+                _picInfo = new ObservableCollection<PicInfo>();
+            }
+        }
+
+        public async void addPicInfo(PicInfo picInfo)
+        {
+            //add picInfo to the ObservableCollection
+            _picInfo.Add(picInfo);
+            await savePicInfoDataAsync();
+        }
+
+        private async Task savePicInfoDataAsync()
+        {
+            //write into the json file replacing existing json file
+            var jsonSerializer = new DataContractJsonSerializer(typeof(ObservableCollection<PicInfo>));
+            using (var stream = await ApplicationData.Current.LocalFolder.OpenStreamForWriteAsync(fileName, CreationCollisionOption.ReplaceExisting))
+            {
+                jsonSerializer.WriteObject(stream, _picInfo);
+            }
+        }
+
+
+    }
+}
---
+++ WeStream/WeStream/StreamPage.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using WeStream.DataModel;
+using Windows.Devices.Geolocation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Media.Capture;
+using Windows.Media.MediaProperties;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
+
+namespace WeStream
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class StreamPage : Page
+    {
+
+
+        public StreamPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            //GPS
+            Geopoint myPoint;
+
+            var locator = new Geolocator();
+            locator.DesiredAccuracyInMeters = 50;
+
+            var position = await locator.GetGeopositionAsync();
+            myPoint = position.Coordinate.Point;
+
+            //Camera
+            MediaCapture mediaCapture;
+
+            mediaCapture = new MediaCapture();
+            await mediaCapture.InitializeAsync();
+            //assign to XAML element and start preview
+            PhotoPreview.Source = mediaCapture;
+            await mediaCapture.StartPreviewAsync();
+
+        }
+
+        public async Task CapturePhoto()
+        {
+            MediaCapture mediaCapture;
+
+            mediaCapture = new MediaCapture();
+
+            //creating jpeg properties
+            var imgEncodingProperties = ImageEncodingProperties.CreateJpeg();
+            imgEncodingProperties.Width = 640;
+            imgEncodingProperties.Height = 480;
+
+            //Create new picture and place into Local, if name existing create unique name
+            var photoStorageFile = await ApplicationData.Current.LocalFolder.CreateFileAsync(titleBox.Text, CreationCollisionOption.GenerateUniqueName);
+
+            await mediaCapture.CapturePhotoToStorageFileAsync(imgEncodingProperties, photoStorageFile);
+        }
+
+        private async void AppBarButton_Click(object sender, RoutedEventArgs e)
+        {
+            PicInfo newPicInfo = new PicInfo();
+            newPicInfo.picTitle = titleBox.Text;
+            newPicInfo.created = DateTime.Now;
+            newPicInfo.Latitude = MyMap.Center.Position.Latitude;
+            newPicInfo.Longitude = MyMap.Center.Position.Longitude;
+            App.DataModel.addPicInfo(newPicInfo);
+            await CapturePhoto();
+        }
+    }
+}
